03667a7acb399023e5f2b7dc045a6a3106b08584
Septs <github@septs.pw>
2017-01-09 18:47:22 +0800

    replace the http proxy implementation

--- proxy_http.go
+++ proxy_http.go
@@ -1,90 +1,45 @@
 package proxyclient
 
 import (
-	"bufio"
 	"crypto/tls"
 	"net"
-	"net/http"
 	"net/url"
+	"github.com/RouterScript/HTTPProxy"
 )
 
 func newHTTPProxyClient(url *url.URL) (Client, error) {
-	tlsConfig := &tls.Config{
-		ServerName:        url.Query().Get("tls-domain"),
-		InsecureSkipVerify:url.Query().Get("tls-insecure-skip-verify") == "true",
+	client := proxy.Client{
+		Proxy:       *url,
+		TLSConfig:   &tls.Config{
+			ServerName:        url.Query().Get("tls-domain"),
+			InsecureSkipVerify:url.Query().Get("tls-insecure-skip-verify") == "true",
+		},
+		UpstreamDial:net.Dial,
 	}
-	client := &httpProxyClient{url, tlsConfig}
-	return client, nil
+	return &httpProxyClient{client}, nil
 }
 
 type httpProxyClient struct {
-	url       *url.URL
-	tlsConfig *tls.Config
+	client proxy.Client
 }
 
-func (client *httpProxyClient) Dial(network, address string) (net.Conn, error) {
+func (c *httpProxyClient) Dial(network, address string) (net.Conn, error) {
 	if isTCPNetwork(network) {
-		return client.dialTCP(network, address)
+		return c.client.Dial(network, address)
 	}
 	if isUDPNetwork(network) {
-		return client.DialUDP(network, nil, nil)
+		return c.DialUDP(network, nil, nil)
 	}
 	return nil, ErrUnsupportedNetwork
 }
 
-func (client *httpProxyClient) DialTCP(network string, localAddr, remoteAddr *net.TCPAddr) (net.Conn, error) {
+func (c *httpProxyClient) DialTCP(network string, localAddr, remoteAddr *net.TCPAddr) (net.Conn, error) {
 	if localAddr != nil || localAddr.Port != 0 {
 		return nil, ErrUnsupportedLocalAddr
 	}
-	return client.dialTCP(network, remoteAddr.String())
+	return c.client.Dial(network, remoteAddr.String())
 }
 
-func (client *httpProxyClient) DialUDP(network string, localAddr, remoteAddr *net.UDPAddr) (net.Conn, error) {
+func (c *httpProxyClient) DialUDP(network string, localAddr, remoteAddr *net.UDPAddr) (net.Conn, error) {
 	return nil, ErrUnsupportedProtocol
-}
-
-func (client *httpProxyClient) dialTCP(network, address string) (net.Conn, error) {
-	conn, err := net.Dial(network, client.url.Host)
-	if err != nil {
-		return nil, err
-	}
-	if client.url.Scheme == "https" {
-		tlsConn := tls.Client(conn, client.tlsConfig)
-		if err := tlsConn.Handshake(); err != nil {
-			tlsConn.Close()
-			return nil, err
-		}
-		verifyHostname := tlsConn.VerifyHostname(client.tlsConfig.ServerName)
-		verifyInsecureSkip := client.tlsConfig.InsecureSkipVerify
-		if !verifyInsecureSkip && verifyHostname != nil {
-			tlsConn.Close()
-			return nil, verifyHostname
-		}
-		conn = tlsConn
-	}
-	remoteHost, _, err := net.SplitHostPort(address)
-	if err != nil {
-		return nil, err
-	}
-	request, err := http.NewRequest(http.MethodConnect, remoteHost, nil)
-	if err != nil {
-		conn.Close()
-		return nil, err
-	}
-	request.URL.Host = address
-	request.Host = address
-	if err := request.Write(conn); err != nil {
-		conn.Close()
-		return nil, err
-	}
-	reader := bufio.NewReader(conn)
-	response, err := http.ReadResponse(reader, request)
-	if err != nil {
-		conn.Close()
-		return nil, err
-	}
-	if response.StatusCode != http.StatusOK {
-		conn.Close()
-	}
-	return conn, nil
 }
