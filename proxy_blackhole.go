83518aaf2722566c6a7916b9128cd01601604efe
Septs <github@septs.pw>
2017-01-12 15:55:54 +0800

    modify blackhole

--- proxy_blackhole.go
+++ proxy_blackhole.go
@@ -18,7 +18,7 @@ type blackholeConn struct{}
 
 func (conn blackholeConn) Read([]byte) (int, error)           { return 0, nil }
 func (conn blackholeConn) Write([]byte) (int, error)          { return 0, nil }
-func (conn blackholeConn) Close() error                       { return errors.New("unsupported") }
+func (conn blackholeConn) Close() error                       { return nil }
 func (conn blackholeConn) LocalAddr() net.Addr                { return nil }
 func (conn blackholeConn) RemoteAddr() net.Addr               { return nil }
 func (conn blackholeConn) SetDeadline(t time.Time) error      { return errors.New("unsupported") }
